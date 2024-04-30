d9fa60670ebb8ba3c5c3a885c6609e2d0a99c6d0
Sara <saradidouh22@gmail.com>
2024-04-30 19:49:08 +0200

    muchos componentes y páginas nuevas .)

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,10 +1,21 @@
+import { Routes, Route } from "react-router-dom"
+import Home from './components/Home'
 import Header from './components/Header'
+import AboutUs from './components/AboutUs'
+import Events from './components/Events'
 import './index.css'
 import './App.css'
 
 function App() {
   return (
+    <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/AboutUs" element={ <AboutUs /> } />
+        <Route path="/Events" element={ <Events /> } />
+      </Routes>
+    </div>
   )
 }
 
