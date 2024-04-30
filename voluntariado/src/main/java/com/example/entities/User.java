e0d9aadc208c0310e6ada1a5af1cbc441e2c01c1
David Baeza <davidbc_98@hotmail.com>
2024-04-30 17:12:07 +0200

    Cambios password seguridad

--- voluntariado/src/main/java/com/example/entities/User.java
+++ voluntariado/src/main/java/com/example/entities/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Data
 // Creates getters and setters automatically
@@ -23,6 +25,57 @@ public class User {
     private String username;
     private Boolean organizer;
     private String entity;
+    
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		this.password = encoder.encode(password);
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Boolean getOrganizer() {
+		return organizer;
+	}
+	public void setOrganizer(Boolean organizer) {
+		this.organizer = organizer;
+	}
+	public String getEntity() {
+		return entity;
+	}
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
     
   
     
