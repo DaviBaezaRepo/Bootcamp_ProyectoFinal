dceaf5deba8b03f66a84a62ce1ef7b178d17d014
David Baeza <davidbc_98@hotmail.com>
2024-05-02 16:10:54 +0200

    Añadiendo get put etc a events mas pruebas

--- voluntariado/src/main/java/com/example/entities/controller/EventController.java
+++ voluntariado/src/main/java/com/example/entities/controller/EventController.java
@@ -1,9 +1,15 @@
 package com.example.entities.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +21,36 @@ import com.example.service.EventService;
 public class EventController {
 	
 	@Autowired
-	EventService eventService;
+	private EventService eventService;
 	
 	@GetMapping("/events")
 	public List<Event> getEvents() {
 		return eventService.findAllEvents();
+	}
+	
+	@GetMapping(path = "/events/{id}")
+    public Optional<Event> getEventById(@PathVariable Long id) {
+        return eventService.getById(id);
+    }
+	
+	@PostMapping("/events")
+	public Event saveEvent(@RequestBody Event event) {
+		return this.eventService.saveEvent(event);
+	}
+	
+	@PutMapping("/events/{id}")
+	public Event updateEventById(@RequestBody Event request, Long id) {
+		return this.eventService.updateById(request, id);
+	}
+	
+	@DeleteMapping("/events/{id}")
+	public String deleteEventById(@PathVariable Long id) {
+		boolean ok = this.eventService.deleteEvent(id);
+		if(ok) {
+			return "Event with id "+id+" deleted.";
+		}else {
+			return "Event with id "+id+" not deleted.";
+		}
 	}
 	
 }
--- voluntariado/src/main/java/com/example/service/EventService.java
+++ voluntariado/src/main/java/com/example/service/EventService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,4 +17,32 @@ public class EventService {
 	public List<Event> findAllEvents() {
 		return eventRepository.findAll()	;
 	}
+	 public Optional<Event> getById(Long id) {
+	        return eventRepository.findById(id);
+	    }
+	 
+	 public Event saveEvent(Event event) {
+	    	return eventRepository.save(event);
+	    }
+	 
+	 public Event updateById(Event request, Long id) {
+	    	Event event = eventRepository.findById(id).get();
+	    	event.setTitle(request.getTitle());
+	    	event.setDescription(request.getDescription());
+	    	event.setImage(request.getImage());
+	    	event.setLocation(request.getLocation());
+	    	event.setDuration(request.getDuration());
+	    	event.setDateandtime(request.getDateandtime());
+	    	event.setCategories(request.getCategories());
+	    	return event;
+	    }
+	  
+	    public Boolean deleteEvent(Long id) {
+	    	try {
+	    		eventRepository.deleteById(id);
+	    		return true;
+	    	} catch (Exception e) {
+	    		return false;
+	    	}
+	    }
 }
