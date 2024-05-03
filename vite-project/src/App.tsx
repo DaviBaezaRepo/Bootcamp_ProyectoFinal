95904044a04a3a7c2595cf4d8e40f6b65d198d83
Sara <saradidouh22@gmail.com>
2024-05-03 16:17:44 +0200

    added / and /home to routes + added (temporary) footer + legal docs + about the project

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom"
 import Home from './components/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import AboutUs from './components/AboutUs'
 import Events from './components/Events'
 import Login from './components/Login'
+import AboutProject from './components/AboutProject'
 import './index.css'
 import './App.css'
 
@@ -14,11 +16,14 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={ <Home /> } />
+          <Route path="/Home" element={ <Home /> } />
           <Route path="/AboutUs" element={ <AboutUs /> } />
+          <Route path="/AboutProject" element={ <AboutProject/> } />
           <Route path="/Events" element={ <Events /> } />
           <Route path="/Login" element={ <Login /> } />
         </Routes>
       </main>
+      <Footer />
     </div>
   )
 }
--- vite-project/src/components/AboutUs.tsx
+++ vite-project/src/components/AboutUs.tsx
@@ -30,30 +30,7 @@ export default () => {
 
     return (
         <section className="py-14">
-            <div className="max-w-screen-xl mx-auto px-4 text-center md:px-8">
-                {/*<div className="max-w-xl mx-auto mb-14">
-                    <h3 className="text-3xl font-semibold sm:text-4xl mb-7">
-                        Acerca de BetterWorld
-                    </h3>
-                    <p className="text-gray-600 mt-3">
-                    Bienvenido a <span className="green-text">BetterWorld</span>, una plataforma dedicada a fomentar la solidaridad y el bienestar comunitario. En BetterWorld, creemos en el poder de la colaboración y la acción colectiva para generar un impacto positivo en nuestra sociedad.
-                    </p>
-                    <h4>Nuestra Misión</h4>
-                    <p className="text-gray-600 mt-3">
-                    Nuestra misión en BetterWorld es conectar a personas con iniciativas solidarias y facilitar su participación en proyectos que contribuyan al bienestar de nuestra comunidad. Nos esforzamos por ofrecer una plataforma accesible y fácil de usar que inspire y movilice a individuos y organizaciones para trabajar juntos en la construcción de un mundo más solidario y compasivo.                                    
-                    </p>
-                    <h4>Nuestros Valores</h4>
-                    <p className="text-gray-600 mt-3">
-                    En BetterWorld, nos guiamos por los siguientes valores fundamentales:
-                    </p>
-                    <li className="text-gray-600 mt-3">
-                        <ul>Solidaridad: Creemos en la solidaridad como fuerza motriz para el cambio social positivo. Trabajamos para fomentar la empatía y el apoyo mutuo en nuestra comunidad.</ul>
-                        <ul>Inclusión: Nos esforzamos por crear un espacio inclusivo donde todas las personas, independientemente de su origen, género, orientación sexual, capacidad o situación socioeconómica, se sientan bienvenidas y valoradas.</ul>
-                        <ul>Transparencia: Promovemos la transparencia y la honestidad en todas nuestras acciones. Nos comprometemos a mantener una comunicación clara y abierta con nuestra comunidad.</ul>
-                        <ul>Responsabilidad: Aspiramos a ser responsables en nuestras acciones y decisiones. Nos comprometemos a utilizar nuestros recursos de manera responsable y a actuar con integridad en todo momento.</ul>
-                    </li>
-                </div>*/}                
-                
+            <div className="max-w-screen-xl mx-auto px-4 text-center md:px-8">           
                 {/* The team */}
                 <div className="max-w-xl mx-auto">
                     <h3 className="text-3xl font-semibold sm:text-4xl mb-7">
