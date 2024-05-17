675379bf9b5b25ed013e79f3889fb24075242706
Sara <saradidouh22@gmail.com>
2024-05-17 19:34:04 +0200

    plantillas de mis eventos y eventos guardados

--- vite-project/src/components/SavedEvents.tsx
+++ vite-project/src/components/SavedEvents.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+export default () => {
 
-const SavedEvents = () => {
-    return <h2>Eventos guardados</h2>;
-};
-
-export default SavedEvents;
+    return (
+        <div className="max-w-screen-xl mx-auto mt-8 px-4 md:px-8">
+            <div className="items-start justify-between md:flex">
+                <div className="max-w-lg">
+                    <h2 className="text-gray-800 font-medium font-bold sm:text-2xl">
+                        Eventos a los que asisto
+                    </h2>
+                </div>
+            </div>
+        </div>
+    )
+}
